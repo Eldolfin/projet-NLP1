ec52d6e68e685e6a0b6bc2ca3249ae0f56f8e0fc
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2025-03-03 16:19:10 +0100

    add dataset

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (
+              python313.withPackages
+              (
+                ppkgs:
+                  with ppkgs; [
+                    datasets
+                    jupyter-all
+                    notebook
+                    nltk
+                    numpy
+                    snakeviz
+                    python-lsp-server
+                    python-lsp-ruff
+                    pylsp-rope
+                    pylsp-mypy
+                    pyls-isort
+
+                    black
+                    graphviz
+                    ipdb
+                    isort
+                    jupyterlab
+                    kneed
+                    matplotlib
+                    numpy
+                    opencv-python
+                    optuna
+                    pandas
+                    pudb
+                    scikit-learn
+                    scipy
+                    seaborn
+                    snakeviz
+                    termcolor
+                    torch
+                    wikipedia
+                  ]
+              )
+            )
+          ];
+        };
+      }
+    );
+}
